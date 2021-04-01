165b84ccfa68a59a37b1ad7c93d327fbd4f9295c
kuzmich321 <dota2drot@gmail.com>
2021-04-01 07:54:12 +0000

    * decorators: decorator_app_dispatching updated

--- decorators/decorator_app_dispatching.py
+++ decorators/decorator_app_dispatching.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from html import escape
+from numbers import Integral
+from collections.abc import Sequence
 
 
 def html_escape(arg):
@@ -41,8 +43,8 @@ def htmlize(arg):
 	registry = {
 		object: html_escape,
 		int: html_int,
-		float: html_int,
-		Decimal: html_int,
+		float: html_real,
+		Decimal: html_real,
 		str: html_str,
 		list: html_list,
 		tuple: html_list,
@@ -64,3 +66,101 @@ print(htmlize(["""Python
 rocks!
 """, (10, 20, 30), 100]))
 print('---------------')
+
+
+def single_dispatch(fn):
+	registry = {}
+
+	registry[object] = fn
+	registry[int] = lambda a: f'{a}(<i>{str(hex(a))}</i>)'
+	registry[str] = lambda s: html_escape(s).replace('\n', '<br/>\n')
+
+	def decorator(arg):
+		return registry.get(type(arg), registry[object])(arg)
+	return decorator
+
+
+@single_dispatch
+def htmlize(a):
+	return escape(str(a))
+
+
+print(htmlize('1 < 100'))
+print(htmlize(100))
+print('---------------')
+
+
+def single_dispatch(fn):
+	registry = {}
+
+	registry[object] = fn
+
+	def decorator(arg):
+		return registry.get(type(arg), registry[object])(arg)
+
+	def register(type_):
+		def inner(fn):
+			registry[type_] = fn
+			return fn
+		return inner
+
+	def dispatch(type_):
+		return registry.get(type_, registry[object])
+
+	decorator.register = register
+	# decorator.registry = registry
+	decorator.dispatch = dispatch
+	return decorator
+
+
+@single_dispatch
+def htmlize(a):
+	return escape(str(a))
+
+
+print(htmlize('1 < 100'))
+print(htmlize(100))
+
+
+@htmlize.register(int)
+def html_int(a):
+	return f'{a}(<i>{str(hex(a))}</i>)'
+
+
+print(htmlize(100))
+
+
+@htmlize.register(set)
+@htmlize.register(tuple)
+@htmlize.register(list)
+def html_sequence(l):
+	items = (f'<li>{html_escape(item)}</li>' for item in l)
+	return '<ul>\n' + '\n'.join(items) + '\n</ul>'
+
+
+print(htmlize([1, 2, 3]))
+print(htmlize((1, 2, 3)))
+print(htmlize({3, 2, 1}))
+
+print(htmlize.dispatch(int))
+print(htmlize.dispatch(frozenset))
+print('---------------')
+
+
+@single_dispatch
+def htmlize(a):
+	return escape(str(a))
+
+
+# @htmlize.register(Integral)
+# def html_integral_number(a):
+# 	return f'{a}(<i>{str(hex(a))}</i>)'
+#
+#
+# print(htmlize(10))  # It fails
+#
+#
+# @htmlize.reigster(Sequence)
+# def html_sequence(l):
+# 	items = (f'<li>{html_escape(item)}</li>' for item in l)
+# 	return '<ul>\n' + '\n'.join(items) + '\n</ul>'
